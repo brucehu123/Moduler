3c263b8d7fa7fee6744dcff98a53f5615dd07df4
bruce_hu <bruce_hu123@163.com>
2019-07-24 21:18:25 +0800

    一些修改

---
+++ Yuhu.Infrastructure/Modules/AbstractModule.cs
@@ -0,0 +1,68 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yuhu.Infrastructure.Modules
+{
+    public abstract class AbstractModule : Autofac.Module, IDisposable
+    {
+        #region 实例属性
+
+        public ContainerBuilderWrapper Builder { get; set; }
+
+        public Guid Identifier { get; set; }
+
+
+        public string ModuleName { get; set; }
+
+
+        public string TypeName { get; set; }
+
+
+        public string Title { get; set; }
+
+        public bool Enable { get; set; } = true;
+
+
+        public string Description { get; set; }
+
+
+        #endregion
+
+        public AbstractModule()
+        {
+            ModuleName = this.GetType().Name;
+            TypeName = this.GetType().BaseType.FullName;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            try
+            {
+                base.Load(builder);
+                Builder = new ContainerBuilderWrapper(builder);
+                if (Enable)
+                {
+                    RegisterServices(Builder);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        //注册服务
+        public virtual void RegisterServices(ContainerBuilderWrapper builder)
+        {
+
+        }
+    }
+}
---
+++ Yuhu.Infrastructure/Modules/IModuleProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yuhu.Infrastructure.Modules
+{
+    public interface IModuleProvider
+    {
+        List<AbstractModule> Modules { get; }
+    }
+}
